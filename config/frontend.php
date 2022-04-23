7e3d54db937d42d5e4ed46171ffe2518da2e088d
mayu <mayu6v0@me.com>
2022-04-23 23:43:21 +0900

    fix

--- config/frontend.php
+++ config/frontend.php
@@ -1,6 +1,6 @@
 <?php
 return [
-  'url' => env('FRONTEND_URL', 'https://rese-next.herokuapp.com'),
+  'url' => env('FRONTEND_URL', 'https://rese-nuxt.herokuapp.com'),
   // 'reset_pass_url' => env('RESET_PASS_URL', '/reset?queryURL='),
   'email_verify_url' => env('FRONTEND_EMAIL_VERIFY_URL', '/auth/emailverification?queryURL='),
 ];
