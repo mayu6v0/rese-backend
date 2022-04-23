7d9df4ebbdfd704faabc8d8b0f1e279ce3544765
mayu <mayu6v0@me.com>
2022-04-23 22:56:53 +0900

    frontend-url追加

--- config/frontend.php
+++ config/frontend.php
@@ -1,6 +1,6 @@
 <?php
 return [
-  'url' => env('FRONTEND_URL', 'http://localhost:3000'),
+  'url' => env('FRONTEND_URL', 'https://rese-next.herokuapp.com'),
   // 'reset_pass_url' => env('RESET_PASS_URL', '/reset?queryURL='),
-  'email_verify_url' => env('FRONTEND_EMAIL_VERIFY_URL', '/verify?queryURL='),
+  'email_verify_url' => env('FRONTEND_EMAIL_VERIFY_URL', '/auth/emailverification?queryURL='),
 ];
