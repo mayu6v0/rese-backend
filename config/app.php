dc591bf161d15fc40bf7db359874126a878eeb74
mayu <mayu6v0@me.com>
2022-04-24 09:34:58 +0900

    frontend_URL変更

--- config/app.php
+++ config/app.php
@@ -54,9 +54,9 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
-
-    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
+    'frontend_url' => env('FRONTEND_URL', 'https://rese-nuxt.herokuapp.com'),
+    
+    'email_verify_url' => env('FRONTEND_EMAIL_VERIFY_URL', '/auth/emailverification?queryURL='),
 
     'asset_url' => env('ASSET_URL', null),
 
